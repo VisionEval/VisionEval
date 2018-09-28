18da5ebdda8967133a664e5291cd7d839e0508a9
Jeremy Raw <jeremy@jeremyraw.com>
2018-09-28 15:39:28 -0400

    Updates to build functions

---
+++ install/supply/build-miniCRAN.R
@@ -0,0 +1,87 @@
+# This script will install R packages in a repository
+# MiniCRAN: https://docs.microsoft.com/en-us/sql/advanced-analytics/r/create-a-local-package-repository-using-minicran?view=sql-server-2017
+# Drat: http://dirk.eddelbuettel.com/code/drat.html
+# Packrat/RStudio: https://rstudio.github.io/packrat/custom-repos.html
+# Script requirements:
+#	1. Build a local repository with all dependencies
+#	2. Make it available for installation (can GitHub serve up pre-packages .zip's? - use GitHub's "release" function)
+#	3. Allow the installation to use an online R repository (hosted, e.g., at visioneval.jeremyraw.com)
+#		a. Use this script to create the repository, then move it to the server
+#	4. Build an end-user script in R to manage the following:
+#		a. Location of "repository" (local folder, or internet URL - default to internet, but point at downloaded .zip file)
+#		b. Location of VisionEval installation folder (create if necessary)
+#		c. Firing up VisionEval (e.g. within R, using a visioneval() function)
+#	5. Package all of that up in a single end-user package, which has one function "visioneval()" that:
+#		a. Checks for installed visioneval in the current working directory (skip to "c" if already present)
+#			i. e.g. present user with "visioneval" as a subdirectory of the current one
+#			ii. if no directory found, or it doesn't contain VisionEval, offer to install
+#		b. Offers to install:
+#			i. From Github release (large binary)
+#			ii. From local file
+#			iii. From VisionEval repository (e.g. at visioneval.jeremyraw.com)
+#			iv. Or search for installed visioneval on current computer
+#		c. Launches a startup page from the visioneval subdirectory
+		
+# How about this:
+#   install_github builds the visioneval package (so kind of a hybrid approach with WSP/RSG)
+#   then you get the visioneval package in place and it has a visioneval() function that retrieves the rest.
+#   You can use that function to install from github (using install.R), from visioneval online repo, or from local miniCRAN repository
+#   We could also add in a build function that will (a) retrieve missing stuff from github; (b) build everything
+
+require(miniCRAN)
+
+# Base R packages (so we can ignore those as dependencies)
+pkgs.BaseR <- as.vector(installed.packages(priority=c("base","recommended"))[,"Package"])
+
+# miniCRAN additions (we'll keep these, as well as miniCRAN itself)
+pkgs.miniCRAN <- miniCRAN::pkgDep("miniCRAN",repos="https://cran.rstudio.org",suggests=FALSE)
+
+# CRAN Dependencies
+pkgs.CRAN <- c(
+"curl",
+"devtools",
+"roxygen2",
+"stringr",
+"knitr",
+"digest",
+"shiny",
+"shinyjs",
+"shinyFiles",
+"data.table",
+"DT",
+"shinyBS",
+"future",
+"testit",
+"jsonlite",
+"shinyAce",
+"envDocument",
+"rhandsontable",
+"shinyTree"
+)
+pkgs.CRAN <- miniCRAN::pkgDep(pkgs.CRAN)
+pkgs.all <- setdiff(unique(c(pkgs.CRAN,pkgs.miniCRAN)),pkgs.BaseR)
+
+# TODO: include the install_github NamedCapture package
+# Possibly the easiest way - just grab the repository and build the package,
+# then place the built files directly in the "built" folders
+# Possibly a script to download the repository and build the package...
+# It is only used in VEGUI to report status messages - has to be an easier way
+
+# Now move all the CRAN packages into miniCRAN
+path.miniCRAN = "N:/Git-Repos/miniCRAN"
+dir.create(path.miniCRAN)
+miniCRAN::makeRepo(pkgs.all,path=path.miniCRAN,repos="https://cran.rstudio.org",type=c("source","win.binary"))
+
+# Finally, add the BioConductor packages to miniCRAN (this will pull a few additional dependencies from CRAN itself)
+pkgs.BioC <- c("rhdf5","zlibbioc")
+
+# Get the BioConductor magic that manages their repositories
+# They have their own installation program to manage dependencies across repositories
+# The package descriptions say where to look, and miniCRAN knows about that...
+bioc <- local({
+  env <- new.env()
+  on.exit(rm(env))
+  evalq(source("http://bioconductor.org/biocLite.R", local = TRUE), env)
+  biocinstallRepos()
+})
+miniCRAN::addPackage(pkgs.BioC,path=path.miniCRAN,repos=bioc,type=c("source","win.binary"))
--- install/supply/build-packages.R
+++ install/supply/build-packages.R
@@ -1,23 +1,28 @@
 # Build and (optionally) Check the VE packages
-# paramter "repo.root" should be set to the root folder of the current repository clone
+# paramter "ve.root" should be set to the root folder of the current repository clone
 # Set up an .Rprofile in this folder and source it (e.g. by starting a fresh R)
 
-#TODO: consider building source into "built/src" and binary into "built/contrib/windows/..."
-if (!is.defined(repo.root))
-	repo.root <- choose.dir(getwd(),caption="Locate Repository Root Directory")
-if (is.na(repo.root))
+if (!exists("ve.root") || !file.exists(ve.root) )
+	ve.root <- choose.dir(getwd(),caption="Locate Repository Root Directory")
+if (!exists("ve.root") || is.na(ve.root) || !file.exists(ve.root) ) # NA generated if user cancels choose.dir
 	stop("Can't build packages without knowing the repository")
-build.dir <- file.path(repo.root,"install","built")
-framework.dir <- file.path(repo.root,"sources","framework")
-modules.dir <- file.path(repo.root,"sources","modules")
-built.path <- file.path(repo.root,"install","built")
+
+# Where to find the package sources
+framework.dir <- file.path(ve.root,"sources","framework")
+modules.dir <- file.path(ve.root,"sources","modules")
+
+# Where to put the built results (these are not version controlled, so create if missing)
+dir.create( built.path <- file.path(ve.root,"install","built"), showWarnings=FALSE )
+dir.create( built.path.src <- file.path(built.path,"src"), showWarnings=FALSE )
+dir.create( built.path.binary <- file.path(built.path,"bin"), showWarnings=FALSE )
 
 # Create module manifest
 # NOTE: there is an order dependency for building/checking modules
-# NOTE: list formatted here to make it easy to stuff in another module
-framework <- c("visioneval")
+# NOTE: list formatted here to make it easy to add another module
+# TODO: create a manifest file in "sources/modules" with all module names in build order
+framework <- file.path(framework.dir,"visioneval")
 modules <- c(
-	"VE2001NHTS"
+	 "VE2001NHTS"
 	,"VESyntheticFirms"
 	,"VESimHouseholds"
 	,"VELandUse"
@@ -34,17 +39,19 @@ build.modules <- file.path(modules.dir,modules)
 require(devtools)
 
 # Check the packages
-if (askYesNo("Comprehensively check packages (Warning: PAINFUL)")) {
-	devtools::check(file.path(framework.dir,framework),path=built.path,binary=TRUE)
-	for (module in build.modules) devtools::check(module,path=built.path,binary=TRUE)
+if (askYesNo("Comprehensively check packages (Warning: PAINFUL)",default=FALSE)) {
+	devtools::check(framework)
+	for (module in build.modules) devtools::check(module)
 }
 
 # Build the framework and modules as source packages
-devtools::build(file.path(framework.dir,framework),path=built.path)
-for (module in build.modules) devtools::build(module,path=built.path)
+if (askYesNo("Build source packages)",default=TRUE)) {
+	devtools::build(framework,path=built.path.src)
+	for (module in build.modules) devtools::build(module,path=built.path.src)
+}
 
 # Build the framework and modules as binary packages (note - side effect is to install them)
-if (askYesNo("Build binary packages (Warning: SLOW)")) {
-	devtools::build(file.path(framework.dir,framework),path=built.path,binary=TRUE)
-	for (module in build.modules) devtools::build(module,path=built.path,binary=TRUE)
+if (askYesNo("Build binary packages (Warning: SLOW)",default=FALSE)) {
+	devtools::build(framework,path=built.path.binary,binary=TRUE)
+	for (module in build.modules) devtools::build(module,path=built.path.binary,binary=TRUE)
 }
