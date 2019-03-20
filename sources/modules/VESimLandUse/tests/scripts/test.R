c7d490ccfef2b62ebcfd9af43f81487c2d7b043e
Jeremy Raw <jeremy.raw@dot.gov>
2019-03-20 13:27:41 -0400

    Disabled VE-State test for VESimLandUse - missing datastore.tar

--- sources/modules/VESimLandUse/tests/scripts/test.R
+++ sources/modules/VESimLandUse/tests/scripts/test.R
@@ -2,5 +2,8 @@
 #------
 #Test script which calls scripts for testing modules of different models
 
-#Test for VERSPM
-source("tests/scripts/vestate_test.R")
+#Test for VE-State
+#source("tests/scripts/vestate_test.R")
+
+cat("Missing Datastore.tar for VE-State models (in Test_Data - see VERSPM)\n")
+cat("No tests executed for VESimLandUse\n")
