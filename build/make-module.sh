0badea95f23fd80845688ba47cb09c64a04f44e6
Jeremy Raw <jeremy.raw@dot.gov>
2019-03-20 10:30:07 -0400

    [skip travis] Added resiliency in make-modules.sh for missing test.R Update .travis.yml with VETravelPerformance dependency on VEPowerTrainsandFuels Fixed typo in VESimLandUse test.R

--- build/make-module.sh
+++ build/make-module.sh
@@ -9,13 +9,17 @@ then
   export R_LIBS_USER=${BUILD_LIB}:${R_LIBS_USER}
 fi
 
-echo Operating in $(pwd)
 pushd $1
 TEST_SCRIPT=${2:-${VE_SCRIPT:-tests/scripts/test.R}}
 echo TEST_SCRIPT=${TEST_SCRIPT}
 Rscript -e "devtools::check('.')"
-echo Executing ${TEST_SCRIPT} in $(pwd)
-Rscript -e "tryCatch( source('${TEST_SCRIPT}') )"
-echo Installing to ${BUILD_LIB}
+if [ -f ${TEST_SCRIPT} ]
+then
+	echo Executing ${TEST_SCRIPT} in $(pwd)
+	Rscript -e "tryCatch( source('${TEST_SCRIPT}') )"
+else
+	echo ${TEST_SCRIPT} does not exist in $(pwd)
+fi
+echo Installing '$(basename $(pwd))' to ${BUILD_LIB}
 R CMD INSTALL -l "${BUILD_LIB}" . # Save the installed package for later use
 popd
--- sources/modules/VESimLandUse/tests/scripts/test.R
+++ sources/modules/VESimLandUse/tests/scripts/test.R
@@ -3,4 +3,4 @@
 #Test script which calls scripts for testing modules of different models
 
 #Test for VERSPM
-source("tests/scripts/verspm_test.R")
+source("tests/scripts/vestate_test.R")
