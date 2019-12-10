0ea7836cbbef916844062de7c2213bf00798c879
gregorbj <gregorbj@gmail.com>
2019-12-10 09:58:03 -0800

    Change ISELEMENTOF Get spec for StateAbbrLookup to be consistent with specs in Initialize and CalculateRoadDvmt modules

--- sources/modules/VETravelPerformance/R/CalculateRoadPerformance.R
+++ sources/modules/VETravelPerformance/R/CalculateRoadPerformance.R
@@ -1163,12 +1163,7 @@ CalculateRoadPerformanceSpecifications <- list(
       TYPE = "character",
       UNITS = "ID",
       PROHIBIT = "",
-      ISELEMENTOF = c(
-        "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI",
-        "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI",
-        "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
-        "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
-        "VT", "VA", "WA", "WV", "WI", "WY", "DC", "PR", NA)
+      ISELEMENTOF = ""
     ),
     item(
       NAME = "Marea",
