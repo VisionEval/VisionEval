fcd3399da24c9128047b6c02a38041ee5341f67d
gregorbj <gregorbj@gmail.com>
2020-06-05 12:29:39 -0700

    Modify the 'assignHhUrbanDvmtProp' function that is called in the 'CalculateRoadDvmt' module to handle the case where an azone is part of an marea, but none of the households in the azone are located in the urban portion of the marea.

--- sources/modules/VETravelPerformance/R/CalculateRoadDvmt.R
+++ sources/modules/VETravelPerformance/R/CalculateRoadDvmt.R
@@ -5,7 +5,7 @@
 #<doc>
 #
 ## CalculateRoadDvmt Module
-#### January 27, 2019
+#### June 5, 2020
 #
 #When run for the base year, this module computes several factors used in computing roadway DVMT including factors for calculating commercial service vehicle travel and heavy truck travel. While base year heavy truck DVMT and commercial service vehicle DVMT are calculated directly from inputs and model parameters, future year DVMT is calculated as a function of the declared growth basis which for heavy trucks may be population or income, and for commercial service vehicles may be population, income, or household DVMT. Factors are calculated for each basis. In non-base years, the module uses these factors to compute heavy truck and commercial service DVMT.
 #
@@ -579,20 +579,26 @@ assignHhUrbanDvmtProp <- function(Hh_ls, HhUrbanRoadDvmt_Ma) {
       Dvmt_ <- Hh_ls$Dvmt[Hh_ls$Marea == ma]
       LocType_ <- Hh_ls$LocType[Hh_ls$Marea == ma]
       Prob_ <- as.numeric(LocType_ == "Urban")
-      UrbanRoadProp_ <- Prob_ * HhUrbanRoadDvmt_Ma[ma] / sum(Dvmt_ * Prob_)
-      if (any(UrbanRoadProp_ > 1)) {
+      if (any(Prob_ == 1)) {
+        UrbanRoadProp_ <- Prob_ * HhUrbanRoadDvmt_Ma[ma] / sum(Dvmt_ * Prob_)
+        if (any(UrbanRoadProp_ > 1)) {
+          UrbanRoadProp_[UrbanRoadProp_ > 1] <- 1
+          RoadDvmtDiff <- HhUrbanRoadDvmt_Ma[ma] - sum(UrbanRoadProp_ * Dvmt_)
+          UrbanRoadProp_[LocType_ != "Urban"] <-
+            RoadDvmtDiff / sum(Dvmt_[LocType_ != "Urban"])
+        }
         UrbanRoadProp_[UrbanRoadProp_ > 1] <- 1
-        RoadDvmtDiff <- HhUrbanRoadDvmt_Ma[ma] - sum(UrbanRoadProp_ * Dvmt_)
-        UrbanRoadProp_[LocType_ != "Urban"] <-
-          RoadDvmtDiff / sum(Dvmt_[LocType_ != "Urban"])
+        HhDvmt_ <- tapply(Dvmt_, LocType_, sum)
+        HhUrbanDvmt_ <- tapply(UrbanRoadProp_ * Dvmt_, LocType_, sum)
+        Prop_[Hh_ls$Marea == ma] <- UrbanRoadProp_
+        UrbanHhPropUrbanDvmt_Ma[ma] <- HhUrbanDvmt_["Urban"] / HhDvmt_["Urban"]
+        NonUrbanHhPropUrbanDvmt_Ma[ma] <-
+          (sum(HhUrbanDvmt_) - HhUrbanDvmt_["Urban"])  / (sum(HhDvmt_) - HhDvmt_["Urban"])
+      } else {
+        Prop_[Hh_ls$Marea == ma] <- HhUrbanRoadDvmt_Ma[ma] / sum(Dvmt_)
+        UrbanHhPropUrbanDvmt_Ma[ma] <- 0
+        NonUrbanHhPropUrbanDvmt_Ma[ma] <- HhUrbanRoadDvmt_Ma[ma] / sum(Dvmt_)
       }
-      UrbanRoadProp_[UrbanRoadProp_ > 1] <- 1
-      HhDvmt_ <- tapply(Dvmt_, LocType_, sum)
-      HhUrbanDvmt_ <- tapply(UrbanRoadProp_ * Dvmt_, LocType_, sum)
-      Prop_[Hh_ls$Marea == ma] <- UrbanRoadProp_
-      UrbanHhPropUrbanDvmt_Ma[ma] <- HhUrbanDvmt_["Urban"] / HhDvmt_["Urban"]
-      NonUrbanHhPropUrbanDvmt_Ma[ma] <-
-        (sum(HhUrbanDvmt_) - HhUrbanDvmt_["Urban"])  / (sum(HhDvmt_) - HhDvmt_["Urban"])
     } else {
       Prop_[Hh_ls$Marea == ma] <- 0
       UrbanHhPropUrbanDvmt_Ma[ma] <- 0
