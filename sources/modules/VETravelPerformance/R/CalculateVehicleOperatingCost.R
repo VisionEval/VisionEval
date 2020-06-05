d0c6c1b36054fb55facbfebf21882b002b0eefc5
gregorbj <gregorbj@gmail.com>
2020-06-05 12:40:42 -0700

    Change Set specs for AveVehCostPM, AveSocEnvCostPM, AveRoadUseTaxPM, AveGPM, AveKWHPM, and AveCO2ePM to allow NA values. NA values will exist for any households that are group quarters persons 0-14 years old. They have no vehicle acces and hence the values will be NA for those 'households'.

--- sources/modules/VETravelPerformance/R/CalculateVehicleOperatingCost.R
+++ sources/modules/VETravelPerformance/R/CalculateVehicleOperatingCost.R
@@ -5,7 +5,7 @@
 #<doc>
 #
 ## CalculateHhVehicleOperatingCosts Module
-#### January 23, 2019
+#### June 5, 2020
 #
 #This module calculates vehicle operating costs per mile of travel and uses those costs to determine the proportional split of DVMT among household vehicles. The module also calculates the average out-of-pocket costs per mile of vehicle travel by household, as well as the cost of social and environmental impacts, and road use taxes per mile of vehicle travel.
 #
@@ -966,7 +966,7 @@ CalculateVehicleOperatingCostSpecifications <- list(
       TYPE = "currency",
       UNITS = "USD.2010",
       NAVALUE = -1,
-      PROHIBIT = c("NA", "< 0"),
+      PROHIBIT = c("< 0"),
       ISELEMENTOF = "",
       SIZE = 0,
       DESCRIPTION = "Average out-of-pocket cost in dollars per mile of vehicle travel"
@@ -978,7 +978,7 @@ CalculateVehicleOperatingCostSpecifications <- list(
       TYPE = "currency",
       UNITS = "USD.2010",
       NAVALUE = -1,
-      PROHIBIT = c("NA", "< 0"),
+      PROHIBIT = c("< 0"),
       ISELEMENTOF = "",
       SIZE = 0,
       DESCRIPTION = "Average cost in dollars of the social and environmental impacts per mile of vehicle travel"
@@ -990,7 +990,7 @@ CalculateVehicleOperatingCostSpecifications <- list(
       TYPE = "currency",
       UNITS = "USD.2010",
       NAVALUE = -1,
-      PROHIBIT = c("NA", "< 0"),
+      PROHIBIT = c("< 0"),
       ISELEMENTOF = "",
       SIZE = 0,
       DESCRIPTION = "Average road use taxes in dollars collected per mile of vehicle travel"
@@ -1014,7 +1014,7 @@ CalculateVehicleOperatingCostSpecifications <- list(
       TYPE = "compound",
       UNITS = "GGE/MI",
       NAVALUE = -1,
-      PROHIBIT = c("NA", "< 0"),
+      PROHIBIT = c("< 0"),
       ISELEMENTOF = "",
       SIZE = 0,
       DESCRIPTION = "Average gasoline equivalent gallons per mile of household vehicle travel"
@@ -1026,7 +1026,7 @@ CalculateVehicleOperatingCostSpecifications <- list(
       TYPE = "compound",
       UNITS = "KWH/MI",
       NAVALUE = -1,
-      PROHIBIT = c("NA", "< 0"),
+      PROHIBIT = c("< 0"),
       ISELEMENTOF = "",
       SIZE = 0,
       DESCRIPTION = "Average kilowatt-hours per mile of household vehicle travel"
@@ -1038,7 +1038,7 @@ CalculateVehicleOperatingCostSpecifications <- list(
       TYPE = "compound",
       UNITS = "GM/MI",
       NAVALUE = -1,
-      PROHIBIT = c("NA", "< 0"),
+      PROHIBIT = c("< 0"),
       ISELEMENTOF = "",
       SIZE = 0,
       DESCRIPTION = "Average grams of carbon-dioxide equivalents produced per mile of household vehicle travel"
