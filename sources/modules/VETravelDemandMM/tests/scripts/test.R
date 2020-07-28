916a13c3c4fee1523eca49b21c77e1dac6b0c8bd
Arash <39972246+arashasadabadi@users.noreply.github.com>
2020-07-28 08:59:40 -0700

    Update VETravelDemandMM tests

--- sources/modules/VETravelDemandMM/tests/scripts/test.R
+++ sources/modules/VETravelDemandMM/tests/scripts/test.R
@@ -35,46 +35,68 @@ testModule(
   DoRun = TRUE
 )
 
-#Test PredictAADVMT module
-#source("R/PredictAADVMT.R")
-testModule(
-  ModuleName = "PredictAADVMT",
-  #ProjectDir = TestDir,
-  ParamDir = "defs",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
+# #Test PredictAADVMT module
+# #source("R/PredictAADVMT.R")
+# testModule(
+#   ModuleName = "PredictAADVMT",
+#   #ProjectDir = TestDir,
+#   ParamDir = "defs",
+#   LoadDatastore = TRUE,
+#   SaveDatastore = TRUE,
+#   DoRun = TRUE
+# )
+# 
+# #Test PredictBikePMT module
+# #source("R/PredictBikePMT.R")
+# testModule(
+#   ModuleName = "PredictBikePMT",
+#   #ProjectDir = TestDir,
+#   ParamDir = "defs",
+#   LoadDatastore = TRUE,
+#   SaveDatastore = TRUE,
+#   DoRun = TRUE
+# )
+# 
+# #Test PredictWalkPMT module
+# #source("R/PredictWalkPMT.R")
+# testModule(
+#   ModuleName = "PredictWalkPMT",
+#   #ProjectDir = TestDir,
+#   ParamDir = "defs",
+#   LoadDatastore = TRUE,
+#   SaveDatastore = TRUE,
+#   DoRun = TRUE
+# )
+# 
+# #Test PredictTransitPMT module
+# #source("R/PredictTransitPMT.R")
+# testModule(
+#   ModuleName = "PredictTransitPMT",
+#   #ProjectDir = TestDir,
+#   ParamDir = "defs",
+#   LoadDatastore = TRUE,
+#   SaveDatastore = TRUE,
+#   DoRun = TRUE
+# )
 
-#Test PredictBikePMT module
-#source("R/PredictBikePMT.R")
-testModule(
-  ModuleName = "PredictBikePMT",
+# #Test CalculateHouseholdDvmt module
+# #source("R/CalculateHouseholdDvmt.R")
+ testModule(
+   ModuleName = "CalculateHouseholdDvmt",
   #ProjectDir = TestDir,
   ParamDir = "defs",
   LoadDatastore = TRUE,
   SaveDatastore = TRUE,
   DoRun = TRUE
-)
-
-#Test PredictWalkPMT module
-#source("R/PredictWalkPMT.R")
-testModule(
-  ModuleName = "PredictWalkPMT",
-  #ProjectDir = TestDir,
-  ParamDir = "defs",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Test PredictTransitPMT module
-#source("R/PredictTransitPMT.R")
-testModule(
-  ModuleName = "PredictTransitPMT",
-  #ProjectDir = TestDir,
-  ParamDir = "defs",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
+ )
+ 
+ # #Test CalculateAltModeTrips module
+ # #source("R/CalculateAltModeTrips.R")
+ testModule(
+   ModuleName = "CalculateAltModeTrips",
+   #ProjectDir = TestDir,
+   ParamDir = "defs",
+   LoadDatastore = TRUE,
+   SaveDatastore = TRUE,
+   DoRun = TRUE
+ )
