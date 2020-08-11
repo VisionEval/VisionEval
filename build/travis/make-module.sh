dafe2773b19ed903521c02bde3bda9616dacc5fb
Jeremy Raw <jeremy.raw@dot.gov>
2020-08-11 11:50:39 -0400

    Removed previous test architecture from Travis-CI

--- build/travis/make-module.sh
+++ build/travis/make-module.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Updated 2020-08-11 to remove module unit tests
+# Those simulate a model environment, and we haven't kept up with
+# suitable input/target data in the Test_Data folder under sources/modules
+# Next major release will include new tests and a new testing architecture.
+
 set -ev
 
 BUILD_LIB=${VE_LIBRARY:-$TRAVIS_BUILD_DIR/ve-lib}
@@ -10,16 +16,16 @@ then
 fi
 
 pushd $1
-TEST_SCRIPT=${2:-${VE_SCRIPT:-tests/scripts/test.R}}
-echo TEST_SCRIPT=${TEST_SCRIPT}
+# TEST_SCRIPT=${2:-${VE_SCRIPT:-tests/scripts/test.R}}
+# echo TEST_SCRIPT=${TEST_SCRIPT}
 Rscript -e "devtools::check('.',cran=FALSE,error_on='error')"
-if [ -f ${TEST_SCRIPT} ]
-then
-	echo Executing ${TEST_SCRIPT} in $(pwd)
-	Rscript -e "tryCatch( source('${TEST_SCRIPT}') )"
-else
-	echo ${TEST_SCRIPT} does not exist in $(pwd)
-fi
+# if [ -f ${TEST_SCRIPT} ]
+# then
+# 	echo Executing ${TEST_SCRIPT} in $(pwd)
+# 	Rscript -e "tryCatch( source('${TEST_SCRIPT}') )"
+# else
+# 	echo ${TEST_SCRIPT} does not exist in $(pwd)
+# fi
 echo Installing "$(basename $(pwd))" to ${BUILD_LIB}
 R CMD INSTALL -l "${BUILD_LIB}" . # Save the installed package for later use
 popd
