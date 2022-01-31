16e208a617d8aba4e8b7de25b0860c4b76ee7efc
Jeremy Raw (FHWA) <jeremy.raw@dot.gov>
2022-01-31 17:21:38 -0500

    Added ve.test to dev environemnt; copy tests to runtime

--- build/scripts/build-runtime.R
+++ build/scripts/build-runtime.R
@@ -38,11 +38,39 @@ cat("that.R:",this.R,"\n",sep="",file=file.path(ve.runtime,"r.version"))
 cat("ve.lib.name:",basename(ve.lib),"\n",sep="",file=file.path(ve.runtime,"r.version"),append=TRUE)
 cat("ve.pkgs.name:",basename(ve.pkgs),"\n",sep="",file=file.path(ve.runtime,"r.version"),append=TRUE)
 
-# Process the 'script' and 'model' items
+# Process any framework or module package tests (locate them in the "tools/tests/<Package>" folder
+# Runtime ve.test("<Package>") will attach those scripts  contents into "test:<Package>"
+cat("Setting up Framework and Module tests...\n")
+copy.tests <- pkgs.db[c(pkgs.framework,pkgs.module),]
+copy.tests <- copy.tests[nzchar(copy.tests$Test),c("Root","Path","Package","Test")]
+test.scripts <- data.frame(Root=NA,Path=NA,Package=NA,Test=NA)
+for ( test in 1:nrow(copy.tests) ) {
+  test.files <- unlist(strsplit(copy.tests[test,"Test"],";",fixed=TRUE))
+  test.row <- copy.tests[test,c("Root","Path","Package","Test")]
+  for ( fn in test.files ) {
+    if ( ! grepl("/|\\\\",fn ) ) fn <- file.path("tests",fn)
+    test.row$Test <- fn
+    test.scripts <- rbind(test.scripts,test.row)
+  }
+}
+# Reformat the test.scripts data.frame so we can just treat the tests as scripts...
+test.scripts <- test.scripts[-1, ] # remove dummy row
+if ( nrow(test.scripts) > 0 ) {
+  test.scripts$Target <- file.path("tools/tests",test.scripts$Package)
+  test.scripts$Path <- file.path(test.scripts$Path,test.scripts$Package,dirname(test.scripts$Test))
+  test.scripts$Package <- basename(test.scripts$Test)
+  test.scripts <- test.scripts[,c("Root","Path","Package","Target")]
+}
 
-cat("Script sources...\n")
+# Process the 'script' (and tests, if any) and 'model' items
 
 copy.scripts <- pkgs.db[pkgs.script,c("Root","Path","Package","Target")]
+if ( nrow(test.scripts) > 0 ) {
+  copy.scripts <- rbind(copy.scripts,test.scripts)
+  test.msg <- paste(" (and",nrow(test.scripts),"Test files)")
+} else test.msg <- ""
+cat("Script sources",test.msg,"...\n",sep="")
+
 copy.paths <- file.path(copy.scripts$Root, copy.scripts$Path, copy.scripts$Package)
 if ( length(copy.paths) > 0 ) {
   for ( f in seq_along(copy.paths) ) {
@@ -66,14 +94,22 @@ if ( length(copy.paths) > 0 ) {
     }
     pkg.files <- dir(copy.paths[f],recursive=TRUE,all.files=TRUE) # force deeper check for actual files
     if ( do.copy || (test.newer <- newerThan(copy.paths[f], test.path, pkg.files=pkg.files)) ) {
-      cat("Copying Script", copy.paths[f],"\n")
-      invisible(file.copy(copy.paths[f], target.path, recursive=dir.exists(copy.paths[f])))
+      if ( file.exists(copy.paths[f]) ) {
+        cat("Copying Script", copy.paths[f],"\n")
+        cat("            TO",target.path,"\n")
+        invisible(file.copy(copy.paths[f], target.path, recursive=dir.exists(copy.paths[f])))
+      } else {
+        cat("Configured source file does not exist:",copy.paths[f],"\n")
+      }
     }
   }
 }
 
 cat("Model sources...\n")
-
+# NOTE: as of "Next Gen" 2022, probably only copies a Readme.md telling the user
+# to use VEModel::installModel to populate the "models" directory.
+# We don't need the readme: the standard runtime script (sources/runtime/VisionEval.R) will create
+# the "models" directory if it does not exists.
 copy.models <- pkgs.db[pkgs.model,c("Root","Path","Package")]
 copy.paths <- file.path(copy.models$Root, copy.models$Path, copy.models$Package)
 model.path <- file.path(ve.runtime,"models")
@@ -85,7 +121,7 @@ if ( length(copy.paths) > 0 ) {
     any.newer <- any( any.newer, newer )
   }
   if ( any.newer ) {
-    cat(paste("Copying Models: ", copy.paths),sep="\n")
+    cat(paste("Copying Model: ", copy.paths),sep="\n")
     dir.create( model.path, recursive=TRUE, showWarnings=FALSE )
     invisible(file.copy(copy.paths, model.path, recursive=TRUE))
   } else {
