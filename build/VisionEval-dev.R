30755153c13f0230cb716470c2e3435896fa498d
Jeremy Raw <jeremy@jeremyraw.com>
2022-11-04 14:16:09 -0400

    Fixed build/VisionEval-dev.R to clean up ve.test environments
    Release candidate for VisionEval-dev.R

--- build/VisionEval-dev.R
+++ build/VisionEval-dev.R
@@ -93,16 +93,34 @@ evalq(
       )
     }
 
-    get.ve.runtime <- function(use.git=FALSE,use.env=TRUE) {
+    get.ve.runtime <- function(runtime=NULL, use.git=FALSE,use.env=TRUE) {
+      # Locate dthe working directory in which to look for the VisionEval.R initialization. Options are:
+      #   1. if runtime is an existing directory (could be getwd()), establish that as ve.runtime
+      #   2. Find load.runtime via the "built" tree (for loading VisionEval.R, walkthrough or tests)
+      #      "runtime" directory within the "built" tree (use.git controls how that tree was built)
+      #   2. Set the runtime working directory like this:
+      #      1. Use runtime parameter if provided
+      #      1. Use current ve.runtime in "ve.env" environment, if present
+      #      3. Use VE_RUNTIME environment variable (if set, and use.env is TRUE)
+      #      4. Issue warning about writing in build tree, then use load.runtime
+      #      
       # use.git will use git branch name if TRUE, otherwise "visioneval" for branch
       #   (must be consistent with ve.build(use.git=...)
-      # use.env if TRUE will use system environment VE_RUNTIME (otherwise VE_RUNTIME is ignored)
+      # use.env if TRUE to enable using system environment VE_RUNTIME
+      # (otherwise VE_RUNTIME is ignored; see above)
+
+      # env.builder has information related to building VisionEval
       env.builder <- grep("^ve.bld$",search(),value=TRUE)
       if ( length(env.builder)==0 ) {
         env.builder <- attach(NULL,name="ve.bld")
       } else {
         env.builder <- as.environment(env.builder)
       }
+
+      # runtime parameter is where to do the work:
+      runtime <- if ( ! is.character(runtime) || ! dir.exists(runtime) )
+        NULL else normalizePath(runtime,mustWork=TRUE,winslash="/")
+
       # Load the build file created by last ve.build() (to find ve-lib)
       ve.branch <- getLocalBranch(ve.root,use.git)
       this.r <- paste(R.version[c("major","minor")],collapse=".")
@@ -113,48 +131,135 @@ evalq(
         message("Could not locate built runtime for R-",this.r," in ",build.file,"\n",sep="")
         return(NA)
       }
-      ve.runtime <- if ( exists("ve.runtime",envir=env.builder) ) {
+
+      # load.runtime is location for loading VisionEval.R and r.version
+      # That folder will also locate the walkthrough scripts and tools if they are not present in
+      # the ve.runtime 
+      load.runtime <- if ( exists("ve.runtime",envir=env.builder) ) {
         if ( ! dir.exists(env.builder$ve.runtime) ) {
           message("Runtime directory for ",ve.branch," is missing.")
           NA
         } else get("ve.runtime",pos=env.builder)
       }
 
-      # Now replace the built runtime with VE_RUNTIME if requested to do so and if it is present
-      if ( ! is.na(ve.runtime) && use.env ) {
-        ve.runtime <- Sys.getenv("VE_RUNTIME",env.builder$ve.runtime)
-      }
-      return(structure(ve.runtime,raw.runtime=env.builder$ve.runtime))
-    }
-
-    ve.run <- function(use.git=FALSE,use.env=TRUE) {
-      ve.runtime <- get.ve.runtime(use.git=use.git,use.env=use.env)
-      if ( ! is.na(ve.runtime) && dir.exists(ve.runtime) ) {
-        owd <- setwd(ve.runtime)
-        message("setwd('",owd,"') to return to previous directory")
-        # Read the VisionEval.R startup from the built runtime location even if not running there
-        raw.runtime <- attr(ve.runtime,"raw.runtime")
-        if ( is.null(raw.runtime) ) raw.runtime <- ve.runtime
-        startup.file <- file.path(raw.runtime,"VisionEval.R")
-        if ( file.exists(startup.file) ) {
-          message("setwd(ve.root) to return to Git root directory")
-          message("startup file: ",startup.file)
-          source(startup.file) # Will report directory in which we are running
+      # Now replace the built runtime with runtime parameter or VE_RUNTIME if requested to do so and if it is present
+      ve.runtime <- if ( is.character(runtime) ) {
+        # message("Runtime override: ",runtime)
+        runtime
+      } else if ( use.env ) {
+        env.runtime <- Sys.getenv("VE_RUNTIME",ve.runtime)
+        if ( dir.exists(env.runtime) ) {
+          # message("Explicit VE_RUNTIME exists: ",env.runtime)
+          env.runtime
+        } else {
+          # message("Explicit VE_RUNTIME missing: ",env.runtime)
+          load.runtime
         }
       } else {
-        message("Have you run ve.build()?")
-        owd <- getwd()
+        # "runtime.test" is created as a default "live" runtime by ve.run(0
+        # if there is not better choice.
+        runtime.test <- file.path(dirname(load.runtime),"runtime.test")
+        if ( dir.exists(runtime.test) ) {
+          # message("Using existing runtime.test: ",runtime.test)
+          runtime.test
+        } else {
+          # message("Runtime.test not created yet: ",runtime.test)
+          load.runtime
+        }
       }
-      return( invisible(owd) )
+      
+      return(structure(ve.runtime,load.runtime=load.runtime))
     }
 
-    ve.test <- function(VEPackage,tests="test.R",changeRuntime=TRUE,usePkgload=NULL,...) {
-      # ... parameters are passed to ve.run when changeRuntime is TRUE (use.git or use.env)
+    # runtime says where to set up the "live" runtime
+    # use.git says that the build will be using the git branch name instead of "visioneval"
+    # use.env says to look for VE_RUNTIME in the environment
+    # changeDir says to change the directory to ve.runtime; if FALSE, stay in current directory
+    # copyFiles says to copy the built files from the built runtime to the "working" runtime (e.g. runtime.test)
+    #   copyFiles can be TRUE (copy entire runtime) or a vector of sub-directories of load.runtime to copy
+    #   (e.g. "walkthrough")
+    ve.run <- function(runtime=NULL, use.git=FALSE,use.env=TRUE,changeDir=TRUE,copyFiles=FALSE) {
+      ve.runtime <- get.ve.runtime(runtime, use.git=use.git,use.env=use.env)
+      message("get.ve.runtime returned ",ve.runtime,": use.env ",use.env)
+      if ( is.na(ve.runtime) ) stop("No runtime available. Have you run ve.build()")
+      load.runtime <- attr(ve.runtime,"load.runtime")
+      useWorkingDir <- ! changeDir
+      if ( ! useWorkingDir ) {
+        if ( is.na(ve.runtime) || load.runtime == ve.runtime ) {
+          # Try to avoid running in the built runtime directory
+          runtime.test <- file.path(dirname(load.runtime),"runtime.test")
+          if ( ! dir.exists(runtime.test) ) {
+            message("Creating runtime.test directory:")
+            dir.create(runtime.test)
+          }
+          message("Using runtime test directory:",runtime.test)
+          ve.runtime <- runtime.test
+        } else if ( ! is.na(ve.runtime) && dir.exists(ve.runtime) ) {
+          owd <- setwd(ve.runtime)
+        } else useWorkingDir <- TRUE
+      }
+
+      # Fall back to current directory
+      if ( useWorkingDir ) owd <- ve.runtime <- getwd() else owd <- getwd()
+
+      # Report how to step back to previous directory
+      if ( owd != ve.runtime ) message("setwd('",owd,"') to return to previous directory")
+
+      # Load auxiliary files to the current runtime if requested (will overwrite existing)
+      if ( ( is.character(copyFiles) || isTRUE(copyFiles) ) && load.runtime != ve.runtime ) {
+        if ( is.character(copyFiles ) ) {
+          # copy
+          for ( cf in copyFiles ) {
+            # need to copy all.files to include .Rprofile; full.names to identify which
+            # file and sub-hierarchy to bring across.
+            to.copy <- dir( file.path(load.runtime, cf), all.files=TRUE, full.names=TRUE, no..=TRUE )
+            to.dir <- file.path(ve.runtime,cf)
+            if ( ! dir.exists(to.dir) ) dir.create(to.dir)
+            file.copy( to.copy, to.dir, recursive=TRUE )
+          }
+        } else {
+          to.copy <- dir(load.runtime,all.files=TRUE, full.names=TRUE, no..=TRUE)
+          file.copy( to.copy , ve.runtime, recursive=TRUE )
+          load.runtime <- ve.runtime # Load the startup script from load.runtime
+        }
+      }
+      # In general, there's no need to copy the runtime files over except for
+      # running the tests or the walkthrough. See ve.test implementation below
+
+      # Now run the startup script
+      startup.file <- file.path(load.runtime,"VisionEval.R")
+      if ( file.exists(startup.file) ) {
+        if ( ve.root != owd ) message("setwd(ve.root) to return to Git root directory")
+
+        env.loc <- if ( ! "ve.env" %in% search() ) {
+          attach(NULL,name="ve.env")
+        } else {
+          as.environment("ve.env")
+        }
+        env.loc$ve.runtime <- ve.runtime
+        if ( ve.runtime != getwd() ) setwd(ve.runtime)
+        source(startup.file,chdir=TRUE)
+      } else {
+        message("No startup file in ",ve.load.dir)
+        message("Have you run ve.build()?")
+      }
+
+      return( invisible(structure(ve.runtime,load.runtime=load.runtime)) )
+    }
+
+    ve.test <- function(VEPackage,tests="test.R",changeRuntime=TRUE,usePkgload=NULL,use.git=FALSE,use.env=TRUE) {
+      # Run with the walkthrough if no package provided (or "walkthrough")
+
+      # Then we'll redirect to other locations as needed
       walkthroughScripts = character(0)
       if ( missing(VEPackage) || tolower(VEPackage)=="walkthrough" ) { # load the walkthrough
         if ( changeRuntime ) {
-          ve.run(...)
-          setwd("walkthrough")
+          # If ve.runtime has not moved away from ve.root (i.e. ve.run() was not yet called)
+          # then do the default ve.run first. Otherwise, we'll place the walkthrough in ve.runtime.
+          changeDir <- getwd()==ve.root
+          # Do walkthrough below current runtime directory
+          ve.runtime <- ve.run(changeDir=changeDir,copyFiles="walkthrough",use.git=use.git,use.env=use.env)
+          setwd(file.path(ve.runtime,"walkthrough"))
         } else {
           message("Running in VEModel source (for developing walkthrough)")
           setwd(file.path(ve.root,"sources/framework/VEModel/inst/walkthrough"))
@@ -162,9 +267,11 @@ evalq(
         if ( ! file.exists("00-setup.R") ) {
           stop("No walkthrough 00-setup.R in ",getwd())
         } else {
+          # 00-setup.R will create a temporary runtime in "walkthrough"
           message("Loading walkthrough from ",normalizePath("00-setup.R",winslash="/"))
           source("00-setup.R") # will create shadow runtime directory in "walkthrough"
         }
+        # Running now in temporary runtime of "walkthrough"
         walkthroughScripts = dir("..",pattern="\\.R$",full.names=TRUE)
         if ( is.logical(usePkgload) && usePkgload ) {
           # Do a compatible test load of VEModel itself -- useful for using
@@ -183,9 +290,11 @@ evalq(
           return(invisible(walkthroughScripts))
         }
       } else {
-        ve.runtime <- get.ve.runtime(...) # use standard runtime due to changeRuntime=FALSE
+        # Set the base runtime
+        ve.runtime <- get.ve.runtime(use.git=use.git,use.env=use.env) # use standard runtime for testing another package
       }
 
+      # Make sure pkgload is available
       if ( ! requireNamespace("pkgload",quietly=TRUE) ) {
         stop("Missing required package: 'pkgload'")
       }
@@ -300,7 +409,11 @@ evalq(
       # (Delete and Re-)Create an environment for the package tests ("test.VEPackage) on the search
       # path sys.source each of the test files into that environment
       if ( length(tests) > 0 ) {
-        test.env <- attach(NULL,name="test.VEPackage")
+        env.testPackage <- "test.VEPackage"
+        if ( env.testPackage %in% search() ) {
+          detach(env.testPackage,character.only=TRUE)
+        }
+        test.env <- attach(NULL,name=env.testPackage)
         for ( test in tests ) {
           # Set environment variable with path to test file.
           # Inside the test file that can be used to load auxiliary files
@@ -325,7 +438,8 @@ evalq(
         print( tests )
         return(invisible(tests))
       }
-    }
+    } # end of ve.test function definition
+
     message("ve.build() to (re)build VisionEval\n\t(see build/Building.md for advanced configuration)")
     message("ve.run() to run VisionEval (once it is built)")
     message("ve.test('VEPackage',tests='test.R') to pkgload a VisionEval package and load its tests")
--- sources/runtime/VisionEval.R
+++ sources/runtime/VisionEval.R
@@ -1,11 +1,13 @@
 # Author: Jeremy Raw
 
 # VisionEval runtime initialization script
-# Run once to install everything and build RunVisionEval.Rdata
-# Configure alternate library locations (e.g. development environment
-# through .Renviron): do not rewrite if it already exists. And when
-# starting the VisionEval .Rproj, load up the environment from the
-# project root.
+
+# May need to run a second time to complete installation on Mac or Linux
+
+# The working directory when this script is run is the "load directory"
+# The load directory should have "tools", "walkthrough" and "models" subdirectories
+# "models" will be created if it does not exist.
+# After loading, if there is a ve.runtime set, that will become the "run directory"
 
 require(utils,quietly=TRUE)                 # For install package
 
@@ -16,17 +18,32 @@ env.loc <- if ( ! "ve.env" %in% search() ) {
   as.environment("ve.env")
 }
 
+# Development environment may have set an alternate location
+if ( ! exists("ve.runtime",envir=env.loc) ) {
+  env.loc$ve.runtime <- Sys.getenv("VE_RUNTIME",getwd())
+}
+
+# Set up the load directory
+
+if ( ! exists("ve.load.dir",envir=env.loc,inherits=FALSE ) ) {
+  assign("ve.load.dir",getwd(),envir=env.loc)
+}
+if ( env.loc$ve.load.dir != getwd() ) {
+  message("Loading from other directory: ",ve.load.dir)
+}
+
 # Check the R version (redundant on Windows, but saves having to
 # have a separate VisionEval.R for Linux/Mac)
 
 local({
   ve.incomplete <- "VisionEval environment is unavailable; please re-install."
-  if ( ! file.exists("r.version") ) {
-    message("Missing r.version file.")
+  r.version = file.path(ve.load.dir,"r.version")
+  if ( ! file.exists(r.version) ) {
+    message("Missing r.version file in ",ve.load.dir)
     stop(ve.incomplete)
   }
   ve.vars <- data.frame(
-    scan(file="r.version",
+    scan(file=r.version,
       sep=":",
       what=list( var=character(), var=character() ),
       quiet=TRUE
@@ -51,19 +68,13 @@ local({
   if ( this.R != that.R ) {
     stop("Incorrect R version for this VisionEval installation: expecting R",that.R)
   } else {
-    message("Loading VisionEval for R",this.R,"")
+    message("Loading VisionEval for R",this.R)
   }
 })
 
-# Set the runtime directory
-# Will use the ve.run() directory if running from development environment
-if ( ! exists("ve.runtime") ) {
-  assign("ve.runtime",getwd(),envir=env.loc)
-} else { # otherwise it has probably been set by the development environment
-  assign("ve.runtime",ve.runtime,envir=env.loc)
-}
-
 # Check what installation we need
+# Presume that getwd() is the installed runtime folder, which will either contain or
+# be adjacent to a ve-lib or ve-pkg folder with the required packages.
 
 # Locate ve-lib
 env.loc$checkVE <- function(lib.loc=NULL) {
@@ -73,30 +84,7 @@ env.loc$checkVE <- function(lib.loc=NULL) {
 install.success <- env.loc$checkVE()
 
 if ( ! exists("ve.lib.name") ) ve.lib.name <- "ve-lib"
-ve.lib <- file.path(ve.runtime,ve.lib.name)
-
-# Load tools (helper functions) from their subdirectory
-env.loc$load.helpers <- function() {
-  requireNamespace("import",quietly=TRUE)   # to load the tools
-  ve.tools <- file.path(ve.runtime,"tools",fsep="/")
-  tool.files <- file.path(ve.tools,dir(ve.tools,pattern="\\.R$"),fsep="/")
-  if ( length(tool.files)>0 ) {
-    tools <- character(0)
-    for ( tf in tool.files ) {
-      # Add error checking for tool.contents not present
-      message("Loading tool file: ",tf)
-      try(
-        silent=TRUE,
-        eval(parse(text=paste0("import::here(tool.contents,.from='",tf,"')")))
-      )
-      if ( ! exists("tool.contents") ) next
-      eval(parse(text=paste0("import::into(.into='ve.env',",paste(tool.contents,collapse=","),",.from='",tf,"')")))
-      rm(tool.contents)
-    }
-    rm(tf,tools)
-  }
-  rm(tool.files,ve.tools)
-}
+ve.lib <- file.path(ve.load.dir,ve.lib.name)
 
 # Create function to check or perform installation
 env.loc$ve.install <- if ( ! install.success ) {
@@ -108,7 +96,7 @@ env.loc$ve.install <- if ( ! install.success ) {
     install.success <- env.loc$checkVE(ve.lib)
 
     if ( ! install.success ) {
-      ve.lib.dev <- normalizePath(file.path(ve.runtime,"..",ve.lib.name),winslash="/",mustWork=FALSE)
+      ve.lib.dev <- normalizePath(file.path(ve.load.dir,"..",ve.lib.name),winslash="/",mustWork=FALSE)
       install.success <- env.loc$checkVE(ve.lib.dev)
       if ( install.success ) ve.lib <- ve.lib.dev # use development environment
     } # else ve.lib is working
@@ -116,9 +104,9 @@ env.loc$ve.install <- if ( ! install.success ) {
     if ( ! install.success ) {
       # Attempt to install from package sources in ve-pkg
       if ( ! exists("ve.pkg.name") ) ve.pkg.name <- "ve-pkg"
-      ve.pkg <- file.path(ve.runtime,ve.pkg.name)
+      ve.pkg <- file.path(ve.load.dir,ve.pkg.name)
       if ( ! dir.exists(ve.pkg) ) {
-        ve.pkg.local  <- normalizePath(file.path(ve.runtime,"..",ve.pkg.name),winslash="/",mustWork=FALSE)
+        ve.pkg.local  <- normalizePath(file.path(ve.load.dir,"..",ve.pkg.name),winslash="/",mustWork=FALSE)
         if ( dir.exists(ve.pkg.local) ) ve.pkg <- ve.pkg.local
       }
       if ( ! dir.exists(ve.pkg) ) {
@@ -184,82 +172,111 @@ if ( .Platform$OS.type == 'windows' || install.success ) {
 # Load visioneval
 if ( install.success ) {
   require("VEModel") # load explicitly onto the search path
+
+  # Load tools (helper functions) from their subdirectory in ve.load.dir
+  env.loc$load.helpers <- function() {
+    requireNamespace("import",quietly=TRUE)   # to load the tools
+    ve.tools <- file.path(ve.load.dir,"tools")
+    tool.files <- file.path(ve.tools,dir(ve.tools,pattern="\\.R$"),fsep="/")
+    if ( length(tool.files)>0 ) {
+      tools <- character(0)
+      for ( tf in tool.files ) {
+        # Add error checking for tool.contents not present
+        message("Loading tool file: ",tf)
+        try(
+          silent=TRUE,
+          eval(parse(text=paste0("import::here(tool.contents,.from='",tf,"')")))
+        )
+        if ( ! exists("tool.contents") ) next
+        eval(parse(text=paste0("import::into(.into='ve.env',",paste(tool.contents,collapse=","),",.from='",tf,"')")))
+        rm(tool.contents)
+      }
+      rm(tf,tools)
+    }
+    rm(tool.files,ve.tools)
+  }
   env.loc$load.helpers()
+
+  # create the loadTest function (makes package test functions available)
+  env.loc$loadTest <- function(Package=NULL,files=NULL,clear=FALSE) {
+    test.root <- file.path(ve.load.dir,"tools","tests")
+    if ( !is.character(Package) ) {
+      tests <- dir(test.root,pattern="\\.R$",recursive=TRUE)
+      if ( length(tests) == 0 ) {
+        tests <- "No package tests available"
+      }
+      return(tests)
+    }
+
+    # Environment to receive test functions/objects
+    test.env <- if ( ! "ve.tests" %in% search() ) {
+      attach(NULL,name="ve.tests")
+    } else {
+      as.environment("ve.tests")
+    }
+
+    if ( clear ) {
+      to.clear <- ls(test.env,all=TRUE)
+      if ( length(to.clear)>0 ) rm(list=to.clear,envir=test.env)
+    }
+
+    for ( pkg in Package ) {
+      test.dir <- file.path(test.root,Package)
+      if ( dir.exists(test.dir) ) {
+        if ( !is.character(files) ) {
+          tests <- dir(test.dir,pattern="\\.R$",full.names=TRUE)
+        } else tests <- files
+        for ( test in tests ) {
+          message("Loading tests from ",test," for ",Package)
+          sys.source(test,envir=test.env)
+        }
+      } else {
+        message("No tests available for Package ",Package)
+      }
+    }
+    return( objects(test.env) )
+  }
+
+  # function to set up the walkthrough and its runtime
+  env.loc$walkthrough <- function(reset=FALSE) {
+    # Locate walkthrough directory (sub-directory of ve.runtime)
+    if ( ! dir.exists("walkthrough") ) { # in case we're not in ve.runtime
+      if ( dir.exists(env.loc$ve.runtime) ) {
+        setwd(env.loc$ve.runtime)
+      }
+      if ( ! dir.exists("walkthrough") ) {
+        stop("Walkthrough is not available in ",env.loc$ve.runtime)
+      }
+    }
+    setwd("walkthrough") # Go there
+
+    # Load the setup to create the walkthrough runtime if one is not already present
+    # Will stop in normalizePath if setup.R is not present in getwd()
+    message("setwd(ve.runtime) to exit walkthrough environment")
+    message("Loading walkthrough from ",normalizePath("00-setup.R",winslash="/",mustWork=TRUE))
+    source("00-setup.R") # will stop if we cannot create or change to walkthrough runtime directory
+    walkthroughScripts <- grep("00-setup.R",invert=TRUE,value=TRUE,dir("..",pattern="^[01].*\\.R$",full.names=TRUE))
+    message("Open these script files in order and try out the commands:")
+    print(walkthroughScripts)
+  }
+
+  # Change to the runtime directory
+  if ( dir.exists(env.loc$ve.runtime) ) setwd(env.loc$ve.runtime) else env.loc$ve.runtime <- getwd()
+  message("Running in ",env.loc$ve.runtime)
+
+  # Make sure there is a "Models" directory in the actual runtime folder
+  env.loc$ModelRoot <- file.path(
+    env.loc$ve.runtime,
+    visioneval::getRunParameter("ModelRoot") # Uses runtime configuration or default value "models"
+  )
+  if ( ! dir.exists(env.loc$ModelRoot) ) dir.create(env.loc$ModelRoot,recursive=TRUE,showWarnings=FALSE)
+
 } else {
   # We need the following for the Mac or Linux to do the installation in "user space"
   # and not hang up RStudio for a long time while everything compiles.
   # Making the user call a function is better UX than having them wait for a long time with no
   # explanation
   message("Please run ve.install() to complete installation.")
-}
-
-message("Running in ",ve.runtime)
-
-# Make sure there is a "Models" directory
-env.loc$ModelRoot <- file.path(
-  env.loc$ve.runtime,
-  visioneval::getRunParameter("ModelRoot") # Uses runtime configuration or default value "models"
-)
-if ( ! dir.exists(env.loc$ModelRoot) ) dir.create(env.loc$ModelRoot,recursive=TRUE,showWarnings=FALSE)
-
-# create the LoadTest function (makes package test functions available)
-env.loc$loadTest <- function(Package=NULL,files=NULL,clear=FALSE) {
-  test.root <- file.path(env.loc$ve.runtime,"tools","tests")
-  if ( !is.character(Package) ) {
-    tests <- dir(test.root,pattern="\\.R$",recursive=TRUE)
-    if ( length(tests) == 0 ) {
-      tests <- "No package tests available"
-    }
-    return(tests)
-  }
-
-  # Environment to receive test functions/objects
-  test.env <- if ( ! "ve.tests" %in% search() ) {
-    attach(NULL,name="ve.tests")
-  } else {
-    as.environment("ve.tests")
-  }
-
-  if ( clear ) {
-    to.clear <- ls(test.env,all=TRUE)
-    if ( length(to.clear)>0 ) rm(list=to.clear,envir=test.env)
-  }
-
-  for ( pkg in Package ) {
-    test.dir <- file.path(test.root,Package)
-    if ( dir.exists(test.dir) ) {
-      if ( !is.character(files) ) {
-        tests <- dir(test.dir,pattern="\\.R$",full.names=TRUE)
-      } else tests <- files
-      for ( test in tests ) {
-        message("Loading tests from ",test," for ",Package)
-        sys.source(test,envir=test.env)
-      }
-    } else {
-      message("No tests available for Package ",Package)
-    }
-  }
-  return( objects(test.env) )
-}
-
-# function to set up the walkthrough and its runtime
-env.loc$walkthrough <- function(reset=FALSE) {
-  # Locate walkthrough directory (sub-directory of ve.runtime)
-  if ( ! dir.exists("walkthrough") ) { # in case we're not in ve.runtime
-    setwd(ve.runtime)
-    if ( ! dir.exists("walkthrough") ) {
-      stop("Walkthrough is not available in ",getwd())
-    }
-  }
-  setwd("walkthrough") # Go there
-
-  # Load the setup to create the walkthrough runtime if one is not already present
-  # Will stop in normalizePath if setup.R is not present in getwd()
-  message("Loading walkthrough from ",normalizePath("00-setup.R",winslash="/",mustWork=TRUE))
-  source("00-setup.R") # will stop if we cannot create or change to walkthrough runtime directory
-  walkthroughScripts <- grep("00-setup.R",invert=TRUE,value=TRUE,dir("..",pattern="^[01].*\\.R$",full.names=TRUE))
-  message("Open these script files in order and try out the commands:")
-  print(walkthroughScripts)
 }
 
 # clean up variables created during startup
