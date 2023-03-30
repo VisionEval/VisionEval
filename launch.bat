60709024d39388f5556ee4da4512950f5acf4c2b
Jeremy Raw <jeremy@jeremyraw.com>
2023-03-30 13:16:11 -0400

    Fixed R GUI development launch batch file

--- launch.bat
+++ launch.bat
@@ -1,3 +1,3 @@
 @echo off
-set R_HOME=C:\Users\jeremy.raw\AppData\Local\Programs\R\R-42~1.2
+if "%R_HOME%" == "" set R_HOME=%userprofile%/AppData/Local/Programs/R/R-4.2.3
 start %R_HOME%\bin\x64\RGui.exe --no-save
