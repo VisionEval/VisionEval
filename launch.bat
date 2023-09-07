a54662486c825bdf26edfebd26cf75722bfca543
Jeremy Raw <jeremy@jeremyraw.com>
2023-09-07 15:26:12 -0400

    Build system tweak

--- launch.bat
+++ launch.bat
@@ -1,3 +1,3 @@
 @echo off
-if "%R_HOME%" == "" set R_HOME=%userprofile%/AppData/Local/Programs/R/R-4.2.3
+if "%R_HOME%" == "" set R_HOME=C:/PROGRA~1/R/R-43~1.1
 start %R_HOME%\bin\x64\RGui.exe --no-save
